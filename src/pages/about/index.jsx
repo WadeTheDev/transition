67f439d907faf4f8b98654a90a660da1bd26549a
WadeTheDev <echo1006j@gmail.com>
2024-12-08 01:18:17 +0100

    add curve

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,3 +1,4 @@
+import Curve from '@/components/Layout/Curve'
 import Inner from '@/components/Layout/Inner'
 import Head from 'next/head'
 
@@ -10,13 +11,13 @@ export default function About() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Inner backgroundColor={"#BCF366"}>
+      <Curve >
         <h1>About</h1>
         <div className='body'>
             <p> Nullam mattis mattis dictum. Praesent sit amet condimentum mi, quis venenatis lectus. Phasellus ac ante id purus viverra hendrerit quis at ex. Donec vitae augue pulvinar augue dictum fermentum dapibus nec tellus.</p>
             <p>Phasellus cursus, ante in eleifend vehicula, leo metus mattis est, sit amet dignissim dui nibh in dui. Nullam dictum tortor vitae quam condimentum, eget tincidunt nisi tincidunt. Praesent ut erat at purus gravida aliquam non a purus. Pellentesque sagittis in justo vel venenatis.</p>
         </div>
-      </Inner>
+      </Curve>
     </>
   )
 }
